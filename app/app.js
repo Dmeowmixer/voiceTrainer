24443d4cf37312bc3008baf113a9f8b8d1ed480c
dmeowmixer <dmeowmixer@hotmail.com>
2015-03-18 21:37:36 -1000

    have d3 set up trying to find the data to plug in to d3 , or how to pass sampleRate to the rest of the file

--- app/app.js
+++ app/app.js
@@ -1,315 +1,314 @@
-// if (!window.AudioContext) {
-//   if (! window.webKitAudioContext) {
-//     alert('no AudioContext found :[');
-//   }
-//   window.AudioContext = window.webKitAudioContext;
-// }
-// var context  = new AudioContext();
-// var audioBuffer;
-// var sourceNode;
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-// setupAudioNodes();
-// loadSound("wagner-short.ogg");
+var audioContext = null;
+var isPlaying = false;
+var sourceNode = null;
+var analyser = null;
+var theBuffer = null;
+var DEBUGCANVAS = null;
+var mediaStreamSource = null;
+var detectorElem, 
+  canvasElem,
+  waveCanvas,
+  pitchElem,
+  noteElem,
+  detuneElem,
+  detuneAmount;
 
+window.onload = function() {
+  // Must do this first because everything happens within a audiocontext ( this is a audioNode )
+  audioContext = new AudioContext();
+  // the below code returns a floating point number that represents the sample rate in samples per second used by all nodes in context.
+  MAX_SIZE = Math.max(4,Math.floor(audioContext.sampleRate/5000));  // corresponds to a 5kHz signal
+  var request = new XMLHttpRequest();
+  request.open("GET", "../sounds/whistling3.ogg", true);
+  request.responseType = "arraybuffer";
+  request.onload = function() {
+    audioContext.decodeAudioData( request.response, function(buffer) { 
+        theBuffer = buffer;
+    } );
+  };
+  request.send();
 
-// /* The setupAudioNodes function creates a BufferSource audio node and connects it to the destination.*/
-
-// function setupAudioNodes(){
-//   /* Creates a buffer source node (Research) */
-//   sourceNode = context.createBufferSource();
-//   /* Connects to a destination (Research )*/
-//   sourceNode.connect(context.destination);
-// }
-
-// The loadSound function shows how you can load an audio file. 
-
-// function loadSound(url){
-//   var request = new XMLHttpRequest();
-//   request.open('GET', url,true);
-//   request.responseType = 'arraybuffer';
-
-// // when loaded decode the data (It was coded?)
-
-//   request.onload = function(){
-//   context.decodeAudio(request.response, function(buffer){
-//     // when the audio is decoded play the sound!
-//     playSound(buffer);
-//     }, onError);
-//     request.send();
-//   };
-// }
-
-// /*The buffer which is passed into the playSound function contains decoded audio that can be used by the web audio API.*/
-
-// function playSound(buffer) {
-//     sourceNode.buffer = buffer;
-//     sourceNode.noteOn(0);
-// }
-
-// function onError(e){
-//   console.log(e);
-// }
-
-// fork getUserMedia for multiple browser versions, for those
-// that need prefixes
-
-navigator.getUserMedia = (navigator.getUserMedia ||
-                          navigator.webkitGetUserMedia ||
-                          navigator.mozGetUserMedia ||
-                          navigator.msGetUserMedia);
-
-// set up forked web audio context, for multiple browsers
-// window. is needed otherwise Safari explodes
-
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var voiceSelect = document.getElementById("voice");
-var source;
-var stream;
-
-// grab the mute button to use below
-
-var mute = document.querySelector('.mute');
-
-//set up the different audio nodes we will use for the app
-
-var analyser = audioCtx.createAnalyser();
-analyser.minDecibels = -90;
-analyser.maxDecibels = -10;
-analyser.smoothingTimeConstant = 0.85;
-
-var distortion = audioCtx.createWaveShaper();
-var gainNode = audioCtx.createGain();
-var biquadFilter = audioCtx.createBiquadFilter();
-var convolver = audioCtx.createConvolver();
-
-// distortion curve for the waveshaper, thanks to Kevin Ennis
-// http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
-
-function makeDistortionCurve(amount) {
-  var k = typeof amount === 'number' ? amount : 50,
-    n_samples = 44100,
-    curve = new Float32Array(n_samples),
-    deg = Math.PI / 180,
-    i = 0,
-    x;
-  for ( ; i < n_samples; ++i ) {
-    x = i * 2 / n_samples - 1;
-    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+  detectorElem = document.getElementById( "detector" );
+  canvasElem = document.getElementById( "output" );
+  DEBUGCANVAS = document.getElementById( "waveform" );
+  if (DEBUGCANVAS) {
+    waveCanvas = DEBUGCANVAS.getContext("2d");
+    waveCanvas.strokeStyle = "black";
+    waveCanvas.lineWidth = 1;
   }
-  return curve;
+  pitchElem = document.getElementById( "pitch" );
+  noteElem = document.getElementById( "note" );
+  detuneElem = document.getElementById( "detune" );
+  detuneAmount = document.getElementById( "detune_amt" );
+
+  detectorElem.ondragenter = function () { 
+    this.classList.add("droptarget"); 
+    return false; };
+  detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+  detectorElem.ondrop = function (e) {
+      this.classList.remove("droptarget");
+      e.preventDefault();
+    theBuffer = null;
+
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        // asynchronously decode audio file data contained in an array buffer.
+        audioContext.decodeAudioData( event.target.result, function(buffer) {
+          theBuffer = buffer;
+        }, function(){alert("error loading!");} ); 
+
+      };
+      reader.onerror = function (event) {
+        alert("Error: " + reader.error );
+    };
+      reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+      return false;
+  };
+
+
+
 };
 
-// grab audio track via XHR for convolver node
-
-var soundSource, concertHallBuffer;
-
-ajaxRequest = new XMLHttpRequest();
-
-ajaxRequest.open('GET', 'http://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg', true);
-
-ajaxRequest.responseType = 'arraybuffer';
-
-
-ajaxRequest.onload = function() {
-  var audioData = ajaxRequest.response;
-
-  audioCtx.decodeAudioData(audioData, function(buffer) {
-      concertHallBuffer = buffer;
-      soundSource = audioCtx.createBufferSource();
-      soundSource.buffer = concertHallBuffer;
-    }, function(e){"Error with decoding audio data" + e.err});
-
-  //soundSource.connect(audioCtx.destination);
-  //soundSource.loop = true;
-  //soundSource.start();
+function error() {
+    alert('Stream generation failed.');
+}
+// this gets user media i.e mic input
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+          navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
 }
 
-ajaxRequest.send();
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
-// set up canvas context for visualizer
-
-var canvas = document.querySelector('.visualizer');
-var canvasCtx = canvas.getContext("2d");
-
-var intendedWidth = document.querySelector('.wrapper').clientWidth;
-
-canvas.setAttribute('width',intendedWidth);
-
-var visualSelect = document.getElementById("visual");
-
-var drawVisual;
-
-//main block for doing the audio recording
-
-if (navigator.getUserMedia) {
-   console.log('getUserMedia supported.');
-   navigator.getUserMedia (
-      // constraints - only audio needed for this app
-      {
-         audio: true
-      },
-
-      // Success callback
-      function(stream) {
-         source = audioCtx.createMediaStreamSource(stream);
-         source.connect(analyser);
-         analyser.connect(distortion);
-         distortion.connect(biquadFilter);
-         biquadFilter.connect(convolver);
-         convolver.connect(gainNode);
-         gainNode.connect(audioCtx.destination);
-
-         visualize();
-         voiceChange();
-
-      },
-
-      // Error callback
-      function(err) {
-         console.log('The following gUM error occured: ' + err);
-      }
-   );
-} else {
-   console.log('getUserMedia not supported on your browser!');
-}
-
-function visualize() {
-  WIDTH = canvas.width;
-  HEIGHT = canvas.height;
-
-
-  var visualSetting = visualSelect.value;
-  console.log(visualSetting);
-
-  if(visualSetting == "sinewave") {
+    // Connect it to the destination.
+    // this will create an analyser node which can be used to expose audio time and frequency data to create vis
+    analyser = audioContext.createAnalyser();
     analyser.fftSize = 2048;
-    var bufferLength = analyser.fftSize;
-    console.log(bufferLength);
-    var dataArray = new Uint8Array(bufferLength);
+    mediaStreamSource.connect( analyser );
+    updatePitch();
+}
 
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+function toggleOscillator() {
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( 0 );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+        return "play oscillator";
+    }
+    sourceNode = audioContext.createOscillator();
 
-    function draw() {
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( audioContext.destination );
+    sourceNode.start(0);
+    isPlaying = true;
+    isLiveInput = false;
+    updatePitch();
 
-      drawVisual = requestAnimationFrame(draw);
+    return "stop";
+}
 
-      analyser.getByteTimeDomainData(dataArray);
+function toggleLiveInput() {
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( 0 );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+    }
+    getUserMedia(
+      {
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, gotStream);
+}
 
-      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+function togglePlayback() {
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( 0 );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+        return "start";
+    }
 
-      canvasCtx.lineWidth = 2;
-      canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = theBuffer;
+    sourceNode.loop = true;
 
-      canvasCtx.beginPath();
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( audioContext.destination );
+    sourceNode.start( 0 );
+    isPlaying = true;
+    isLiveInput = false;
+    updatePitch();
 
-      var sliceWidth = WIDTH * 1.0 / bufferLength;
-      var x = 0;
+    return "stop";
+}
 
-      for(var i = 0; i < bufferLength; i++) {
-   
-        var v = dataArray[i] / 128.0;
-        var y = v * HEIGHT/2;
+var rafID = null;
+var tracks = null;
+var buflen = 1024;
+var buf = new Float32Array( buflen );
 
-        if(i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
+var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
-        x += sliceWidth;
-      }
+function noteFromPitch( frequency ) {
+  var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+  return Math.round( noteNum ) + 69;
+}
 
-      canvasCtx.lineTo(canvas.width, canvas.height/2);
-      canvasCtx.stroke();
-    };
+function frequencyFromNoteNumber( note ) {
+  return 440 * Math.pow(2,(note-69)/12);
+}
 
-    draw();
+function centsOffFromPitch( frequency, note ) {
+  return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+}
 
-  } else if(visualSetting == "frequencybars") {
-    analyser.fftSize = 256;
-    var bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
-    var dataArray = new Uint8Array(bufferLength);
 
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+var MIN_SAMPLES = 0;  // will be initialized when AudioContext is created.
 
-    function draw() {
-      drawVisual = requestAnimationFrame(draw);
+function autoCorrelate( buf, sampleRate ) {
+  var SIZE = buf.length;
+  var MAX_SAMPLES = Math.floor(SIZE/2);
+  var best_offset = -1;
+  var best_correlation = 0;
+  var rms = 0;
+  var foundGoodCorrelation = false;
+  var correlations = new Array(MAX_SAMPLES);
 
-      analyser.getByteFrequencyData(dataArray);
-
-      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-      var barWidth = (WIDTH / bufferLength) * 2.5;
-      var barHeight;
-      var x = 0;
-
-      for(var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-
-        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-
-        x += barWidth + 1;
-      }
-    };
-
-    draw();
-
-  } else if(visualSetting == "off") {
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-    canvasCtx.fillStyle = "red";
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+  for (var i=0;i<SIZE;i++) {
+    var val = buf[i];
+    rms += val*val;
   }
+  rms = Math.sqrt(rms/SIZE);
+  if (rms<0.01) // not enough signal
+    return -1;
 
-}
+  var lastCorrelation=1;
+  for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
+    var correlation = 0;
 
-function voiceChange() {
-  distortion.curve = new Float32Array;
-  distortion.oversample = '4x';
-  biquadFilter.gain.value = 0;
-  convolver.buffer = undefined;
+    for (var i=0; i<MAX_SAMPLES; i++) {
+      correlation += Math.abs((buf[i])-(buf[i+offset]));
+    }
+    correlation = 1 - (correlation/MAX_SAMPLES);
+    correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+    if ((correlation>0.9) && (correlation > lastCorrelation)) {
+      foundGoodCorrelation = true;
+      if (correlation > best_correlation) {
+        best_correlation = correlation;
+        best_offset = offset;
+      }
+    } else if (foundGoodCorrelation) {
+      // short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
+      // Now we need to tweak the offset - by interpolating between the values to the left and right of the
+      // best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
+      // we need to do a curve fit on correlations[] around best_offset in order to better determine precise
+      // (anti-aliased) offset.
 
-  var voiceSetting = voiceSelect.value;
-  console.log(voiceSetting);
-
-  if(voiceSetting == "distortion") {
-    distortion.curve = makeDistortionCurve(400);
-  } else if(voiceSetting == "convolver") {
-    convolver.buffer = concertHallBuffer;
-  } else if(voiceSetting == "biquad") {
-    biquadFilter.type = "lowshelf";
-    biquadFilter.frequency.value = 1000;
-    biquadFilter.gain.value = 25;
-  } else if(voiceSetting == "off") {
-    console.log("Voice settings turned off");
+      // we know best_offset >=1, 
+      // since foundGoodCorrelation cannot go to true until the second pass (offset=1), and 
+      // we can't drop into this clause until the following pass (else if).
+      var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];  
+      return sampleRate/(best_offset+(8*shift));
+    }
+    lastCorrelation = correlation;
   }
+  if (best_correlation > 0.01) {
 
+    // It appears that this is the HZ level
+    console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")");
+    return sampleRate/best_offset;
+  }
+  return -1;
+//  var best_frequency = sampleRate/best_offset;
 }
 
-// event listeners to change visualize and voice settings
+function updatePitch( time ) {
+  var cycles = new Array;
+  analyser.getFloatTimeDomainData( buf );
+  var ac = autoCorrelate( buf, audioContext.sampleRate );
 
-visualSelect.onchange = function() {
-  window.cancelAnimationFrame(drawVisual);
-  visualize();
-}
-
-voiceSelect.onchange = function() {
-  voiceChange();
-}
-
-mute.onclick = voiceMute;
-
-function voiceMute() {
-  if(mute.id == "") {
-    gainNode.gain.value = 0;
-    mute.id = "activated";
-    mute.innerHTML = "Unmute";
+  if (ac == -1) {
+    detectorElem.className = "vague";
+    pitchElem.innerText = "--";
+    noteElem.innerText = "-";
+    detuneElem.className = "";
+    detuneAmount.innerText = "--";
   } else {
-    gainNode.gain.value = 1;
-    mute.id = "";    
-    mute.innerHTML = "Mute";
+    detectorElem.className = "confident";
+    pitch = ac;
+    pitchElem.innerText = Math.round( pitch ) ;
+    var note =  noteFromPitch( pitch );
+    noteElem.innerHTML = noteStrings[note%12];
+    var detune = centsOffFromPitch( pitch, note );
+    if (detune == 0 ) {
+      detuneElem.className = "";
+      detuneAmount.innerHTML = "--";
+    } else {
+      if (detune < 0)
+        detuneElem.className = "flat";
+      else
+        detuneElem.className = "sharp";
+      detuneAmount.innerHTML = Math.abs( detune );
+    }
   }
+
+  if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+  rafID = window.requestAnimationFrame( updatePitch );
 }
+
+d3.select("body")
+  .append("canvas")
+  .attr('width', 400)
+  .attr('height', 350);
+
+var data = sampleRate;
+
+var scale = d3.scale.linear()
+  .range([0,100])
+  .domain([1,23]);
+
+data.forEach(function(d, i){
+  context.beginPath();
+  context.rect(scale(d), 150,10,10);
+  context.fillStyle = "red";
+  context.fill();
+  context.closePath();
+});
